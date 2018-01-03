7adf2be5be0bfcadc12e5334cac8bff5bc5cde7b
Daran He <daran@ditto.com>
2018-01-03 14:36:10 -0800

    Adding non root install prefix to cmake to allow make install

--- run_build.sh
+++ run_build.sh
@@ -5,11 +5,16 @@
 # Copyright: 2017 Ditto Technologies. All Rights Reserved.
 # Author: Frankie Li, Daran He
 
+# We need a non-root path for make install and make package.
+INSTALL_DIR=${PWD}/install
+mkdir -p ${INSTALL_DIR}
+
 BUILD_DIR=build
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 cmake .. \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
 	-DBUILD_SHARED_LIBS=ON \
 	-DCPACK_GENERATOR="DEB" \
 	-DCPACK_BINARY_DEB="ON" \
