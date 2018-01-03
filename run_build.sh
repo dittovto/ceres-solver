741b2f33135c93d76fc72e4d34a97b696fe26019
Daran He <daran@ditto.com>
2018-01-03 13:41:50 -0800

    Migrating from aptly to s3. Building both xenial and trusty distros

--- run_build.sh
+++ run_build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #
 # Build script to generate dlib Debian package.
+#
+# Copyright: 2017 Ditto Technologies. All Rights Reserved.
+# Author: Frankie Li, Daran He
 
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+cmake .. \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCPACK_GENERATOR="DEB" \
+	-DCPACK_BINARY_DEB="ON" \
+	-DCPACK_DEBIAN_PACKAGE_SHLIBDEPS="ON" \
+	-DCPACK_PACKAGE_CONTACT="eng@ditto.com"
 
-DLIB_BLD=build
-mkdir -p ${DLIB_BLD}
-cd ${DLIB_BLD}
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCPACK_GENERATOR="DEB" -DCPACK_BINARY_DEB="ON" -DCPACK_DEBIAN_PACKAGE_SHLIBDEPS="ON" -DCPACK_PACKAGE_CONTACT="eng@ditto.com"
-
-make -j 4
-make install
-make package
+make -j$(nproc) install package
