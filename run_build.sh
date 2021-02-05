b41c43a0b84299e8914e255e48facbe378e0d4db
Michael Dippery <mdippery@ditto.com>
2021-02-05 11:00:51 -0800

    Add optional "libcxsparse3" package to dependencies
    On Ubuntu 18.04 and later, the name of "libcxsparse3.1.4" changed to
    "libcxsparse3", so this package should rely on either one of these
    packages in order to work on Ubuntu 18.04. Unfortunately this means we
    have to override the entire dependency list manually. C'est la vie.

--- run_build.sh
+++ run_build.sh
@@ -19,7 +19,6 @@ cmake .. \
 	-DCPACK_PACKAGING_INSTALL_PREFIX="/usr/local" \
 	-DCPACK_GENERATOR="DEB" \
 	-DCPACK_BINARY_DEB="ON" \
-	-DCPACK_DEBIAN_PACKAGE_SHLIBDEPS="ON" \
 	-DCPACK_PACKAGE_CONTACT="eng@ditto.com"
 
 make -j$(nproc) install
